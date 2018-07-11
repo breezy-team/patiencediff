c9d2a6126321461e1bc6918f6065366792561458
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-07-11 21:08:48 +0100

    Import copy of patiencediff from breezy.

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from setuptools import setup, Extension
+
+setup(name="patiencediff",
+      description="Python implementation of the patiencediff algorithm.",
+      version="0.0.1",
+      maintainer_email="Breezy Developers",
+      maintainer="team@breezy-vcs.org",
+      license="GNU GPLv2 or later",
+      url="https://www.breezy-vcs.org/",
+      packages=['patiencediff'],
+      test_suite='patiencediff.test_patiencediff',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Operating System :: POSIX',
+      ],
+      ext_modules=[Extension('patiencediff._patiencediff_c',
+          ['patiencediff/_patiencediff_c.c'])])
