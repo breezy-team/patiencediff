61d0a97f5ad38e45a166d6eaa50f1dbec57137c3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-03 22:44:15 +0000

    Add --pure option to setup.py.

--- setup.py
+++ setup.py
@@ -2,9 +2,32 @@
 # encoding: utf-8
 
 from setuptools import setup, Extension
+from distutils import core
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
+
+ext_modules = [
+    Extension(
+        'patiencediff._patiencediff_c',
+        ['patiencediff/_patiencediff_c.c'])]
+
+
+class Distribution(core.Distribution):
+
+    def is_pure(self):
+        if self.pure:
+            return True
+
+    def has_ext_modules(self):
+        return not self.pure
+
+    global_options = core.Distribution.global_options + [
+        ('pure', None, "use pure Python code instead of C "
+                       "extensions (slower on CPython)")]
+
+    pure = False
+
 
 setup(name="patiencediff",
       description="Python implementation of the patiencediff algorithm.",
@@ -17,6 +40,7 @@ setup(name="patiencediff",
       url="https://www.breezy-vcs.org/",
       packages=['patiencediff'],
       test_suite='patiencediff.test_patiencediff',
+      distclass=Distribution,
       classifiers=[
           'Development Status :: 4 - Beta',
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  # noqa
@@ -27,6 +51,5 @@ setup(name="patiencediff",
           'Programming Language :: Python :: Implementation :: PyPy',
           'Operating System :: POSIX',
       ],
-      ext_modules=[Extension('patiencediff._patiencediff_c',
-          ['patiencediff/_patiencediff_c.c'])]
+      ext_modules=ext_modules,
 )
