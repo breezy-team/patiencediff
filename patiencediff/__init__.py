bf221a4014879065a80ca504c51712f33da3afe6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-10-14 23:39:12 +0000

    Move main function to patiencediff.__main__.

--- patiencediff/__init__.py
+++ patiencediff/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (C) 2005, 2006, 2007 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
@@ -141,33 +140,3 @@ except ImportError:
         recurse_matches_py as recurse_matches,
         PatienceSequenceMatcher_py as PatienceSequenceMatcher
         )
-
-
-def main(args):
-    import optparse
-    p = optparse.OptionParser(usage='%prog [options] file_a file_b'
-                                    '\nFiles can be "-" to read from stdin')
-    p.add_option('--patience', dest='matcher', action='store_const',
-                 const='patience', default='patience',
-                 help='Use the patience difference algorithm')
-    p.add_option('--difflib', dest='matcher', action='store_const',
-                 const='difflib',
-                 default='patience', help='Use python\'s difflib algorithm')
-
-    algorithms = {
-        'patience': PatienceSequenceMatcher,
-        'difflib': difflib.SequenceMatcher}
-
-    (opts, args) = p.parse_args(args)
-    matcher = algorithms[opts.matcher]
-
-    if len(args) != 2:
-        print('You must supply 2 filenames to diff')
-        return -1
-
-    for line in unified_diff_files(args[0], args[1], sequencematcher=matcher):
-        sys.stdout.write(line)
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
