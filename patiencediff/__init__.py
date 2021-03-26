1340b636cc97ee72e1c657b9ff15301a0b001471
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-26 00:19:53 +0000

    Release 0.2.2.

--- patiencediff/__init__.py
+++ patiencediff/__init__.py
@@ -24,7 +24,7 @@ import difflib
 
 __all__ = ['PatienceSequenceMatcher', 'unified_diff', 'unified_diff_files']
 
-__version__ = (0, 2, 1)
+__version__ = (0, 2, 2)
 
 
 # This is a version of unified_diff which only adds a factory parameter
