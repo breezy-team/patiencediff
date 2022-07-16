07872304f6b0eaeb874228c89ee307f7a1fd3887
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-07-16 13:31:48 +0100

    Drop python 2.7 support, move metadata to setup.cfg.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@
 from setuptools import setup, Extension
 from distutils import core
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
-
 ext_modules = [
     Extension(
         'patiencediff._patiencediff_c',
@@ -30,25 +27,7 @@ class Distribution(core.Distribution):
 
 
 setup(name="patiencediff",
-      description="Python implementation of the patiencediff algorithm.",
-      long_description=long_description,
-      version="0.2.2",
-      maintainer="Breezy Developers",
-      maintainer_email="team@breezy-vcs.org",
-      license="GNU GPLv2 or later",
-      url="https://www.breezy-vcs.org/",
       packages=['patiencediff'],
       test_suite='patiencediff.test_patiencediff',
       distclass=Distribution,
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: '
-          'GNU General Public License v2 or later (GPLv2+)',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-          'Operating System :: POSIX',
-      ],
       ext_modules=ext_modules)
