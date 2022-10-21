0207482e012cf332f3c263158005c8212316e46e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-10-21 01:03:41 +0100

    Make C extension mandatory when running in cibuildwheel.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+import os
 from setuptools import setup, Extension
+
 
 ext_modules = [
     Extension(
         'patiencediff._patiencediff_c',
-        ['patiencediff/_patiencediff_c.c'], optional=True)]
+        ['patiencediff/_patiencediff_c.c'],
+        optional=os.environ.get('CIBUILDWHEEL', '0') != '1')]
 
 
 setup(ext_modules=ext_modules)
