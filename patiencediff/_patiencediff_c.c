fcda0a467e01dee4c892716e64b340326acfb235
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-10-26 18:05:07 +0000

    Update copyright headers

--- patiencediff/_patiencediff_c.c
+++ patiencediff/_patiencediff_c.c
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 2007, 2010 Canonical Ltd
+ Copyright (C) 2021-2023 Jelmer Vernooĳ <jelmer@jelmer.uk>
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
